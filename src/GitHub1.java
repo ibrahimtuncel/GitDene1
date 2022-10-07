57a56dcf060a0a7f0a3c8b0ca988b8bee0163d46
ibrahimtuncel <ibrahimtunchel2@gmail.com>
2022-10-08 02:03:52 +0300

    ilk taahhüt

---
+++ src/GitHub1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitHub1 {
+
+public static void main(String[] args) {
+		
+		
+		List<Character> list1 = new ArrayList<>();
+		
+		list1.add('a');
+		list1.add('b');
+		list1.add('c');
+		list1.add('d');
+		list1.add('e');
+		list1.add('g');
+		list1.add('t');
+
+		System.out.println(list1);
+
+	}
+
+}
